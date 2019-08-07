7fcf7c3b98a83030d2998dba419b36f56fd232cf
Zimkazimka <pol2shkan4ik@gmail.com>
2019-08-07 23:28:29 +0300

    fix App styles

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'
 import { Button } from './components/Button';
-
 import { Input } from "./components/Input";
 
 
@@ -16,7 +15,7 @@ class App extends Component {
   return (
       <div className="App">
         <div className='wrapper-calc'>
-            <Input input = {this.state.input}></Input>
+            <Input input = {this.state.input} />
           <div className='row'>
               <Button>7</Button>
               <Button>8</Button>
